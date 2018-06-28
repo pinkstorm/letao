ca607103f918bae8340b721673a6231846411f70
lijing <1595406919@qq.com>
2018-06-28 21:27:53 +0800

    乐淘首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,20 @@
+/**
+ * Created by flyin on 2018/6/28.
+ */
+$(function () {
+
+  //初始化区域滚动
+  mui('.mui-scroll-wrapper').scroll({
+    indicators: false, //是否显示滚动条
+    deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+  });
+
+  //轮播图自动播放
+  //获得slider插件对象
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+    interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+  });
+
+
+})
